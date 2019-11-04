df7d75131eab75af611d6bbccd6e6e965cbf5f3d
atsu0131 <atsushi.isai131@gmail.com>
2019-11-04 17:01:23 +0900

    CI

--- kaikei-laravel/resources/js/App.vue
+++ kaikei-laravel/resources/js/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <h1>Vue Router Demo App</h1>
-
+        <div class="title">
+          <h1>検索サイト</h1>
+          <div class="weather">{{this.city}}{{this.temp}}{{this.condition.main}}</div>
+        </div>
         <p>
             <router-link :to="{ name: 'home' }">Home</router-link> |
             <router-link :to="{ name: 'hello' }">Hello World</router-link>
@@ -13,5 +15,40 @@
     </div>
 </template>
 <script>
-    export default {}
+  import axios from 'axios';
+  export default {
+    data: {
+      city: null, 
+      temp: null, 
+      condition: {
+        main: null
+      }
+    },
+    mounted: function(){
+      axios.get('https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=a2ecb9b147c8f4770d9488c0eb63d070')
+      .then(function(response){
+        this.city = response.data.name
+        this.temp = response.data.main.temp
+        this.condition = response.data.weather[0]
+      }.bind(this))
+      .catch(function(error){
+        console.log(error)
+      })
+    }
+  }
 </script>
+
+<style>
+.title{
+  height: 100px;
+  width: 100%;
+}
+h1{
+  float: left;
+  width:50%;
+}
+.weather{
+  float: right;
+}
+
+</style>
