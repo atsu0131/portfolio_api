fd7b0f71724610665a76172b706a601bfd4ff20f
atsu0131 <atsushi.isai131@gmail.com>
2019-11-14 02:16:42 +0900

    Revert "config修正"
    This reverts commit 38d17e848361b86af7dc7df778484cd6f5282677.

--- kaikei-laravel/app/Http/Controllers/AccountingsController.php
+++ kaikei-laravel/app/Http/Controllers/AccountingsController.php
@@ -13,7 +13,7 @@ class AccountingsController extends Controller
 
     public function getWeatherFromApi()
     {
-      $env = config('app.api_key_weather');
+      $env = env('WEATHER_APP_KEY');
       $url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=";
       $api = $url.$env;
       return $api;
@@ -21,7 +21,7 @@ class AccountingsController extends Controller
 
     public function getdinnerFromApi()
     {
-        $env = config('app.api_key_dinner');
+        $env = env('DINNER_APP_KEY');
         $url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=".$env."&freeword=";
         $api = $url;
         return $api;
