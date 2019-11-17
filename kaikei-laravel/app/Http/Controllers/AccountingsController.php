5e177c82d52616224699139f9aa8eb0d7aea932e
ayasamind <masayaliketennis@yahoo.co.jp>
2019-11-17 17:34:32 +0900

    gazzleを使ってAPIをバックエンドで叩く

--- kaikei-laravel/app/Http/Controllers/AccountingsController.php
+++ kaikei-laravel/app/Http/Controllers/AccountingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class AccountingsController extends Controller
 {
@@ -13,17 +14,25 @@ class AccountingsController extends Controller
 
     public function getWeatherFromApi()
     {
-      $env = env('WEATHER_APP_KEY');
-      $url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=";
-      $api = $url.$env;
-      return $api;
+        $env = env('WEATHER_APP_KEY');
+        $url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=";
+        $api = $url.$env;
+
+        $guzzleClient = new Client();
+        $res = $guzzleClient->get($api);
+        $data = json_decode($res->getBody(),true);
+        return $data;
     }
 
-    public function getdinnerFromApi()
+    public function getDinnerFromApi()
     {
         $env = env('DINNER_APP_KEY');
         $url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=".$env."&freeword=";
         $api = $url;
-        return $api;
+
+        $guzzleClient = new Client();
+        $res = $guzzleClient->get($api);
+        $data = json_decode($res->getBody(),true);
+        return $data;
     }
 }
